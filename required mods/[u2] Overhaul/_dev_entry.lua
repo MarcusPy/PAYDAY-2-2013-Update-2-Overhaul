1e05fa5b39a7cc624206e569c57784c8b9c19db7
MarcusPy <107936046+MarcusPy@users.noreply.github.com>
2025-03-20 20:38:07 +0100

    Delete required mods/[u2] Overhaul directory

--- required mods/[u2] Overhaul/_dev_entry.lua
+++
@@ -1,8 +0,0 @@
-if _G.u2_core.debug.dev_features then
-
-end
-
--- tested features, ready to be made into their own modules / moved from here
-if RequiredScript == "lib/entry" then
-
-end
--- required mods/[u2] Overhaul/localizationmanager.lua
+++
@@ -1,5 +0,0 @@
-if _G.u2_core.debug.string_id_reveal then
-	function LocalizationManager:text(string_id, ...)
-		return string_id
-	end
-end
